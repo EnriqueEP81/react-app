6404414ba0b3ff307237cd284c25a5d523684af5
enrique.estevez.polvorinos.ext <enrique.estevez.polvorinos.ext@ing.com>
2026-04-01 17:27:55 +0200

    menu lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,31 @@
 
 import './styles.css';
+import './App.css';
 import { useState } from 'react';
-import { Board } from "./Board";
-import { findChangedCoord } from './utils';
+import {TicTacToe} from './apps/tictactoe/TicTacToe.jsx'
+
 
 
 function App() {
+  const [activeApp, setActiveApp] = useState('TicTacToe');
   return (
-    <>
-      <h1>Tic, tac, toe</h1>
-      <Game/>
-    </>
+    <div className='app-container'>
+      <div className='sidebar'>
+        <button onClick={() => setActiveApp('TicTacToe')}>Tic Tac Toe</button>
+
+        <button onClick={() => setActiveApp('OtraApp')}>Otra App</button>
+
+      </div>
+      <div className='main-content'>
+          {activeApp ==='TicTacToe' && <TicTacToe/>}
+          {activeApp==='OtraApp' && <div>Proxima App aqui</div>}
+
+        </div>
+    </div>
   )
 }
 
-function Game(){
-  const [history,setHistory] = useState([Array(9).fill(null)]);
-  const [currentMove, setCurrentMove] = useState(0);
-  const [isAsc, setIsasc] = useState(true);
-  const xIsNext = currentMove % 2 === 0;
-  const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares){
-    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
-    setHistory(nextHistory)
-    setCurrentMove(nextHistory.length - 1);
-  }
-
-  function jumpTo(nextMove) {
-    setCurrentMove(nextMove);
-  }
-
-  function setOrder() {
-    setIsasc(!isAsc);
-  }
-
-  const moves = history.map((squares,i) =>{
-
-    const move = isAsc ? i : history.length -1 -i;
-
-    
-    let description;
-    if(move > 0) {
-      const coordChanged = findChangedCoord(history[move-1],history[move]);
-      if (move === history.length -1){
-        description = 'Estas en el movimiento #' + move +' ('+coordChanged.row +','+ coordChanged.col +")";
-        return (
-          <li key={move}>
-            <span>{description}</span>
-          </li>
-        )
-      } else {
-        description = 'Ir al movimiento #' + move + ' ('+coordChanged.row +','+ coordChanged.col +")";
-      }
-    }
-    else {
-      description = 'Ir al movimiento al inicio del juego'
-    }
-
-
-    return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{description}</button>
-      </li>
-    )
-
-  });
-  
-
-  return(
-    <div className='game'>
-      <div className='game-board'>
-          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
-      </div>
-      <div className='game-info'>
-        <button onClick={setOrder}>Cambiar orden</button>
-        <ol>{moves}</ol>
-      </div>
-    </div>
-  );
-}
 
 
 export default App
---
+++ src/apps/tictactoe/TicTacToe.jsx
@@ -0,0 +1,74 @@
+
+import { useState } from 'react';
+import { Board } from "./Board";
+import { findChangedCoord } from './utils';
+
+export function TicTacToe(){
+  const [history,setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const [isAsc, setIsasc] = useState(true);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  function setOrder() {
+    setIsasc(!isAsc);
+  }
+
+  const moves = history.map((squares,i) =>{
+
+    const move = isAsc ? i : history.length -1 -i;
+
+    
+    let description;
+    if(move > 0) {
+      const coordChanged = findChangedCoord(history[move-1],history[move]);
+      if (move === history.length -1){
+        description = 'Estas en el movimiento #' + move +' ('+coordChanged.row +','+ coordChanged.col +")";
+        return (
+          <li key={move}>
+            <span>{description}</span>
+          </li>
+        )
+      } else {
+        description = 'Ir al movimiento #' + move + ' ('+coordChanged.row +','+ coordChanged.col +")";
+      }
+    }
+    else {
+      description = 'Ir al movimiento al inicio del juego'
+    }
+
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+
+  });
+  
+
+  return(
+    <>
+    <h1>Tic, tac, toe</h1>
+    <div className='game'>
+      <div className='game-board'>
+          <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <button onClick={setOrder}>Cambiar orden</button>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+    </>
+  );
+}
